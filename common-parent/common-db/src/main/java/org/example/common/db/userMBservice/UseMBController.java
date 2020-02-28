414c47ab02e72a0b0d01f7414746e98753e0acaa
liyu10 <liyu10@xin.com>
2020-02-29 01:47:17 +0800

    create annotation

---
+++ common-parent/common-db/src/main/java/org/example/common/db/userMBservice/UseMBController.java
@@ -0,0 +1,15 @@
+package org.example.common.db.userMBservice;
+
+import java.lang.annotation.*;
+
+/**
+ * @Description: TODO
+ * @Author: liyu
+ * @Date: 2020/2/29
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
+public @interface UseMBController {
+}
