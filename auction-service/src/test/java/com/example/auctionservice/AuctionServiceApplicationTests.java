e24435bca2454bfcbebda1d67a858242c63d75da
liyu10 <liyu10@xin.com>
2020-02-28 14:53:38 +0800

    create auction server as eureka client

---
+++ auction-service/src/test/java/com/example/auctionservice/AuctionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.auctionservice;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
