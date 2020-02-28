a7fe631d326a0b89e37318a471a19ee74a2a7256
liyu10 <liyu10@xin.com>
2020-02-29 00:39:21 +0800

    set logger for auction-service

--- auction-service/src/main/java/com/example/auctionservice/controller/UserController.java
+++ auction-service/src/main/java/com/example/auctionservice/controller/UserController.java
@@ -1,5 +1,8 @@
 package com.example.auctionservice.controller;
 
+import com.example.auctionservice.entity.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +14,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/user")
 public class UserController {
+
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 }
